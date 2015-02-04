35fc92f7b0884f94233764a14515f050df157a5b
romskom <romskom@gmail.com>
2015-02-04 17:00:32 +0300

    Chapter7 - packages

---
+++ Chapter7/src/packages2.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by rkomarov on 02.02.2015.
+ */
+
+package com.horstmann.impatient
+package people
+
+class Employee (_name: String) {
+  val name: String = _name
+}
+
---
+++ Chapter7/src/packs/PacksApp.scala
@@ -0,0 +1,12 @@
+package packs
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Created by rkomarov on 02.02.2015.
+ */
+object PacksApp extends App {
+
+  val subordinates = new ArrayBuffer[Int]()
+
+}
