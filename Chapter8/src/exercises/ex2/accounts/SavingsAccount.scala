05b4c9da33bcbcb77ca4b193f0d96464ccd94e57
romskom <romskom@gmail.com>
2015-02-06 11:07:15 +0300

    Chapter8 - Exercise 2 : Add SavingsAccount class

---
+++ Chapter8/src/exercises/ex2/accounts/SavingsAccount.scala
@@ -0,0 +1,10 @@
+package exercises.ex2.accounts
+
+import exercises.ex1.accounts.BankAccount
+
+/**
+ * Created by rkomarov on 06.02.2015.
+ */
+class SavingsAccount(initialBalance: Double) extends BankAccount(initialBalance) {
+
+}
