7554c871bd9a69b7ae1d24e4e6d29cd348dbf8f0
Roman Komarov <romskom@gmail.com>
2015-02-14 12:03:38 +0300

    Identifiers

---
+++ Chapter11/src/identifiers/IdentifiersTestApp.scala
@@ -0,0 +1,16 @@
+package identifiers
+
+/**
+ * Created by Роман on 14.02.2015.
+ */
+object IdentifiersTestApp extends App {
+
+  val ** = scala.math.sqrt _
+
+  val result = **(5)
+
+  val `val` = 42
+
+  println(`val`)
+
+}
